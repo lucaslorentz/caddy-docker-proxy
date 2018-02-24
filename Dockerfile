ad5d2423b24c989df91eda5664602733a62af5e9
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-02-24 12:27:21 +0100

    Move main image to docker scratch and improve image versions

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
-FROM alpine:3.7
+FROM alpine:3.7 as alpine
+RUN apk add -U --no-cache ca-certificates
+
+# Image starts here
+FROM scratch
 LABEL maintainer "Lucas Lorentz <lucaslorentzlara@hotmail.com>"
 
 EXPOSE 80 443 2015
+ENV HOME /root
 
-RUN apk add --no-cache ca-certificates curl
+WORKDIR /
+COPY --from=alpine /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
 
 ADD caddy /bin/
 
--- build.sh
+++ build.sh
@@ -9,18 +9,34 @@ go test -race -v $(glide novendor)
 
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o caddy
 
-docker build -t lucaslorentz/caddy-docker-proxy:ci .
+docker build -t lucaslorentz/caddy-docker-proxy:ci -f Dockerfile .
+docker build -t lucaslorentz/caddy-docker-proxy:ci-alpine -f Dockerfile-alpine .
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
     docker login -u lucaslorentz -p "$DOCKER_PASSWORD"
     docker push lucaslorentz/caddy-docker-proxy:ci
+    docker push lucaslorentz/caddy-docker-proxy:ci-alpine
 fi
     
-if [[ "${TRAVIS_TAG}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    export VERSION=$(echo $TAG | cut -c2-)
+if [[ "${TRAVIS_TAG}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]]; then
+    export PATCH_VERSION=$(echo $TRAVIS_TAG | cut -c2-)
+    export MINOR_VERSION=$(echo $PATCH_VERSION | cut -d. -f-2)
+
     docker login -u lucaslorentz -p "$DOCKER_PASSWORD"
+
+    # scratch
     docker tag lucaslorentz/caddy-docker-proxy:ci lucaslorentz/caddy-docker-proxy:latest
-    docker tag lucaslorentz/caddy-docker-proxy:ci lucaslorentz/caddy-docker-proxy:$VERSION
+    docker tag lucaslorentz/caddy-docker-proxy:ci lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}
+    docker tag lucaslorentz/caddy-docker-proxy:ci lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}
     docker push lucaslorentz/caddy-docker-proxy:latest
-    docker push lucaslorentz/caddy-docker-proxy:$VERSION
+    docker push lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}
+    docker push lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}
+
+    # alpine
+    docker tag lucaslorentz/caddy-docker-proxy:ci-alpine lucaslorentz/caddy-docker-proxy:alpine
+    docker tag lucaslorentz/caddy-docker-proxy:ci-alpine lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-alpine
+    docker tag lucaslorentz/caddy-docker-proxy:ci-alpine lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-alpine
+    docker push lucaslorentz/caddy-docker-proxy:alpine
+    docker push lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-alpine
+    docker push lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-alpine
 fi
