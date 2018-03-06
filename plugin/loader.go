b1cc26af363fb05b53a9812273b6320a508576fc
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-03-06 22:19:44 +0100

    Delay loader initialization until caddy calls it

--- plugin/loader.go
+++ plugin/loader.go
@@ -10,19 +10,30 @@ import (
 
 // DockerLoader generates caddy files from docker swarm information
 type DockerLoader struct {
-	Input caddy.CaddyfileInput
+	Input       caddy.CaddyfileInput
+	Initialized bool
 }
 
 // CreateDockerLoader creates a docker loader
 func CreateDockerLoader() *DockerLoader {
-	loader := DockerLoader{
+	return &DockerLoader{
 		Input: caddy.CaddyfileInput{
 			ServerTypeName: "http",
 		},
 	}
-	loader.updateInput()
-	loader.scheduleUpdate()
-	return &loader
+}
+
+// Load returns the current caddy file input
+func (dockerLoader *DockerLoader) Load(serverType string) (caddy.Input, error) {
+	if serverType != "http" {
+		return nil, nil
+	}
+	if !dockerLoader.Initialized {
+		dockerLoader.Initialized = true
+		dockerLoader.updateInput()
+		dockerLoader.scheduleUpdate()
+	}
+	return dockerLoader.Input, nil
 }
 
 func (dockerLoader *DockerLoader) scheduleUpdate() {
@@ -43,16 +54,7 @@ func (dockerLoader *DockerLoader) updateInput() bool {
 
 	dockerLoader.Input.Contents = newContents
 
-	log.Println("[INFO] New CaddyFile:")
-	log.Println(string(dockerLoader.Input.Contents))
+	log.Printf("[INFO] New CaddyFile:\n%s", dockerLoader.Input.Contents)
 
 	return true
-}
-
-// Load returns the current caddy file input
-func (dockerLoader *DockerLoader) Load(serverType string) (caddy.Input, error) {
-	if serverType != "http" {
-		return nil, nil
-	}
-	return dockerLoader.Input, nil
 }
