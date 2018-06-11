277db1b196538f7451876f51d6506c477f811822
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-06-12 01:14:33 +0200

    Add arm32v6 docker images

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ go vet $(glide novendor)
 go test -race -v $(glide novendor)
 
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o caddy
-
 docker build -t lucaslorentz/caddy-docker-proxy:ci -f Dockerfile .
 docker build -t lucaslorentz/caddy-docker-proxy:ci-alpine -f Dockerfile-alpine .
+
+CGO_ENABLED=0 GOARCH=arm GOARM=6 GOOS=linux go build -o caddy
+docker build -t lucaslorentz/caddy-docker-proxy:ci-arm32v6 -f Dockerfile-arm32v6 .
+docker build -t lucaslorentz/caddy-docker-proxy:ci-alpine-arm32v6 -f Dockerfile-alpine-arm32v6 .
--- deploy-branch.sh
+++ deploy-branch.sh
@@ -8,6 +8,8 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
     docker login -u lucaslorentz -p "$DOCKER_PASSWORD"
     docker push lucaslorentz/caddy-docker-proxy:ci
     docker push lucaslorentz/caddy-docker-proxy:ci-alpine
+    docker push lucaslorentz/caddy-docker-proxy:ci-arm32v6
+    docker push lucaslorentz/caddy-docker-proxy:ci-alpine-arm32v6
 else
   echo "Skipping CI deploy"
 fi
