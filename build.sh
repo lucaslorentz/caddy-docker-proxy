d973bf067fe553780f8b9e3c48f3af3729034f21
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-08-29 22:15:12 +0200

    Add test image generation to build script

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ go test -race -v $(glide novendor)
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o caddy
 docker build -t lucaslorentz/caddy-docker-proxy:ci -f Dockerfile .
 docker build -t lucaslorentz/caddy-docker-proxy:ci-alpine -f Dockerfile-alpine .
+docker image tag lucaslorentz/caddy-docker-proxy:ci-alpine lucaslorentz/caddy-docker-proxy:test
 
 CGO_ENABLED=0 GOARCH=arm GOARM=6 GOOS=linux go build -o caddy
 docker build -t lucaslorentz/caddy-docker-proxy:ci-arm32v6 -f Dockerfile-arm32v6 .
