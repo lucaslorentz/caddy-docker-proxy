79063d56287bacecc2a59b0967f57b52aabfce49
ruok5 <brad@fyvm.org>
2019-03-18 21:28:24 -0700

    Add ipfilter plugin

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	_ "github.com/lucaslorentz/caddy-docker-proxy/plugin"
 
 	_ "github.com/caddyserver/dnsproviders/route53"
+	
+	_ "github.com/pyed/ipfilter"
 
 	// Caddy
 	"github.com/mholt/caddy/caddy/caddymain"
