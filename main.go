781362525dd98162da95c3a5ab14e9a3e23f3e50
Lucas Lara <lucaslorentzlara@hotmail.com>
2019-04-29 09:04:21 +0200

    Disable telemetry by default with options to enable it back

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"regexp"
+
 	// Plugins
 	_ "github.com/lucaslorentz/caddy-docker-proxy/plugin"
 
@@ -19,7 +23,20 @@ import (
 	"github.com/mholt/caddy/caddy/caddymain"
 )
 
+var enableTelemetryFlag bool
+var isTrue = regexp.MustCompile("(?i)^(true|yes|1)$")
+
 func main() {
+	flag.BoolVar(&enableTelemetryFlag, "enable-telemetry", false, "Enable caddy telemetry")
+
+	flag.Parse()
+
+	if enableTelemetryEnv := os.Getenv("CADDY_ENABLE_TELEMETRY"); enableTelemetryEnv != "" {
+		caddymain.EnableTelemetry = isTrue.MatchString(enableTelemetryEnv)
+	} else {
+		caddymain.EnableTelemetry = enableTelemetryFlag
+	}
+
 	caddymain.Run()
 
 	// Keep caddy running after main instance is stopped
