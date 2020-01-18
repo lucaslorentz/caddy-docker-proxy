5b2db5da98a5a827d0bc1a176f691c575d35ca09
TheJayMann <themann@indyfantasysports.net>
2020-01-18 18:31:02 -0500

    - Added 1809 image build to build script

--- build-images-windows.sh
+++ build-images-windows.sh
@@ -3,3 +3,4 @@
 set -e
 
 docker build -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803 -f Dockerfile-nanoserver-1803 .
+docker build -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1809 -f Dockerfile-nanoserver-1809 .
