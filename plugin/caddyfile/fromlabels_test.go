2d1a2a527286dfa57bd00af9b54d72e9c6c5d3d8
Francis Lavoie <lavofr@gmail.com>
2020-05-19 01:36:59 -0400

    Refactor tests to use test files for input/expected pairs

--- plugin/caddyfile/fromlabels_test.go
+++ plugin/caddyfile/fromlabels_test.go
@@ -1,175 +1,106 @@
 package caddyfile
 
 import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
 	"testing"
 	"text/template"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFromLabels_Grouping(t *testing.T) {
-	labels := map[string]string{
-		"caddy":               "localhost",
-		"caddy.group.a":       "value-a",
-		"caddy.group.b":       "value-b",
-		"caddy.group.group.a": "value-a",
-		"caddy.group.group.b": "value-b",
+func TestLabelsToCaddyfile(t *testing.T) {
+	// load the list of test files from the dir
+	files, err := ioutil.ReadDir("./testdata/labels")
+	if err != nil {
+		t.Errorf("failed to read labels dir: %s", err)
 	}
 
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
+	// prep a regexp to fix strings on windows
+	winNewlines := regexp.MustCompile(`\r?\n`)
 
-	const expectedCaddyfile = "localhost {\n" +
-		"	group {\n" +
-		"		a value-a\n" +
-		"		b value-b\n" +
-		"		group {\n" +
-		"			a value-a\n" +
-		"			b value-b\n" +
-		"		}\n" +
-		"	}\n" +
-		"}\n"
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
 
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
+		// read the test file
+		filename := f.Name()
+		data, err := ioutil.ReadFile("./testdata/labels/" + filename)
+		if err != nil {
+			t.Errorf("failed to read %s dir: %s", filename, err)
+		}
+
+		// split the labels (first) and Caddyfile (second) parts
+		parts := strings.Split(string(data), "----------")
+		labelsString, expectedCaddyfile := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
+
+		// parse label key-value pairs
+		labels, err := parseLabelsFromString(labelsString)
+		if err != nil {
+			t.Errorf("failed to parse labels from %s", filename)
+		}
+
+		// replace windows newlines in the json with unix newlines
+		expectedCaddyfile = winNewlines.ReplaceAllString(expectedCaddyfile, "\n")
+
+		// convert the labels to a Caddyfile
+		caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
+
+		// if the result is nil then we expect an empty Caddyfile
+		if caddyfileBlock == nil {
+			if expectedCaddyfile != "" {
+				t.Errorf("got nil in %s but expected: %s", filename, expectedCaddyfile)
+			}
+			continue
+		}
+
+		// if caddyfileBlock is not nil, we expect no error
+		assert.NoError(t, err, "expected no error in %s", filename)
+
+		// compare the actual and expected Caddyfiles
+		actualCaddyfile := strings.TrimSpace(caddyfileBlock.MarshalString())
+		assert.Equal(t, expectedCaddyfile, actualCaddyfile,
+			"comparison failed in %s: \nExpected:\n%s\n\nActual:\n%s\n",
+			filename, expectedCaddyfile, actualCaddyfile)
+	}
 }
 
-func TestFromLabels_FollowAlphabeticalOrder(t *testing.T) {
-	labels := map[string]string{
-		"caddy":     "localhost",
-		"caddy.bbb": "value",
-		"caddy.aaa": "value",
+func parseLabelsFromString(s string) (map[string]string, error) {
+	labels := make(map[string]string)
+
+	lines := strings.Split(s, "\n")
+	lineNumber := 0
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		lineNumber++
+
+		// skip lines starting with comment
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		// skip empty line
+		if len(line) == 0 {
+			continue
+		}
+
+		fields := strings.SplitN(line, "=", 2)
+		if len(fields) != 2 {
+			return nil, fmt.Errorf("can't parse line %d; line should be in KEY = VALUE format", lineNumber)
+		}
+
+		key := strings.TrimSpace(fields[0])
+		val := strings.TrimSpace(fields[1])
+
+		if key == "" {
+			return nil, fmt.Errorf("missing or empty key on line %d", lineNumber)
+		}
+		labels[key] = val
 	}
 
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = "localhost {\n" +
-		"	aaa value\n" +
-		"	bbb value\n" +
-		"}\n"
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
-}
-
-func TestFromLabels_OrderDirectivesWithPrefix(t *testing.T) {
-	labels := map[string]string{
-		"caddy":       "localhost",
-		"caddy.1_bbb": "value",
-		"caddy.2_aaa": "value",
-	}
-
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = "localhost {\n" +
-		"	bbb value\n" +
-		"	aaa value\n" +
-		"}\n"
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
-}
-
-func TestFromLabels_SeparateDirectivesWithSuffix(t *testing.T) {
-	labels := map[string]string{
-		"caddy":           "localhost",
-		"caddy.group_1.a": "value",
-		"caddy.group_2.b": "value",
-	}
-
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = "localhost {\n" +
-		"	group {\n" +
-		"		a value\n" +
-		"	}\n" +
-		"	group {\n" +
-		"		b value\n" +
-		"	}\n" +
-		"}\n"
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
-}
-
-func TestFromLabels_TemplatesEmptyValues(t *testing.T) {
-	labels := map[string]string{
-		"caddy":     "localhost",
-		"caddy.key": `{{""}}`,
-	}
-
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = "localhost {\n" +
-		"	key\n" +
-		"}\n"
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
-}
-
-func TestFromLabels_TemplateError(t *testing.T) {
-	labels := map[string]string{
-		"caddy.key": "{{invalid}}",
-	}
-
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = ""
-
-	assert.Error(t, err)
-	assert.Nil(t, caddyfileBlock)
-}
-
-func TestFromLabels_GlobalDirectives(t *testing.T) {
-	labels := map[string]string{
-		"caddy.key": "value",
-	}
-
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = "key value\n"
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
-}
-
-func TestFromLabels_SnippetsComesFirst(t *testing.T) {
-	labels := map[string]string{
-		"caddy_0":        "aaa.com",
-		"caddy_0.import": "my-snippet",
-		"caddy_1":        "(my-snippet)",
-		"caddy_1.tls":    "internal",
-	}
-
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = "(my-snippet) {\n" +
-		"	tls internal\n" +
-		"}\n" +
-		"aaa.com {\n" +
-		"	import my-snippet\n" +
-		"}\n"
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
-}
-
-func TestFromLabels_MatchersComesFirst(t *testing.T) {
-	labels := map[string]string{
-		"caddy":               "localhost",
-		"caddy.@matcher.path": "/path1 /path2",
-		"caddy.respond":       "@matcher 200",
-	}
-
-	caddyfileBlock, err := FromLabels(labels, nil, template.FuncMap{})
-
-	const expectedCaddyfile = "localhost {\n" +
-		"	@matcher {\n" +
-		"		path /path1 /path2\n" +
-		"	}\n" +
-		"	respond @matcher 200\n" +
-		"}\n"
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCaddyfile, caddyfileBlock.MarshalString())
+	return labels, nil
 }
