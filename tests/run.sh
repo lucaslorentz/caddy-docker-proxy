71185f2479d48696a1b92588c4e426f668ea183d
Lucas Lara <lucaslorentzlara@hotmail.com>
2020-06-13 20:35:29 +0200

    Improve integration test

--- tests/run.sh
+++ tests/run.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+docker network create --driver overlay --attachable caddy_test || true
+
 for d in */
 do
+  docker stack rm caddy_test || true
+
   echo ""
   echo ""
   echo "=== Running test $d ==="
