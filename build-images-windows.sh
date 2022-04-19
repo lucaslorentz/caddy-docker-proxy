0ed0db8f19204d819a7e85885fa80c59e9b82b4c
Lucas Lara <lucaslorentzlara@hotmail.com>
2022-04-19 23:10:37 +0200

    Update windows images and try to run integration tests on windows

--- build-images-windows.sh
+++ build-images-windows.sh
@@ -3,19 +3,23 @@
 set -e
 
 docker build -f Dockerfile-nanoserver . \
-    --build-arg NANOSERVER_VERSION=1803 \
-    -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803
-
-docker build -f Dockerfile-nanoserver . \
+    --build-arg TARGETPLATFORM=windows/amd64 \
+    --build-arg SERVERCORE_VERSION=1809 \
     --build-arg NANOSERVER_VERSION=1809 \
     -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1809
+
+docker build -f Dockerfile-nanoserver . \
+    --build-arg TARGETPLATFORM=windows/amd64 \
+    --build-arg SERVERCORE_VERSION=ltsc2022 \
+    --build-arg NANOSERVER_VERSION=ltsc2022 \
+    -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-ltsc2022
 
 if [[ "${BUILD_SOURCEBRANCH}" == "refs/heads/master" ]]; then
     echo "Pushing CI images"
     
     docker login -u lucaslorentz -p "$DOCKER_PASSWORD"
-    docker push lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803
     docker push lucaslorentz/caddy-docker-proxy:ci-nanoserver-1809
+    docker push lucaslorentz/caddy-docker-proxy:ci-nanoserver-ltsc2022
 fi
 
 if [[ "${RELEASE_VERSION}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]]; then
@@ -26,14 +30,6 @@ if [[ "${RELEASE_VERSION}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]]; then
 
     docker login -u lucaslorentz -p "$DOCKER_PASSWORD"
 
-    # nanoserver-1803
-    docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803 lucaslorentz/caddy-docker-proxy:nanoserver-1803
-    docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803 lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-nanoserver-1803
-    docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803 lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-nanoserver-1803
-    docker push lucaslorentz/caddy-docker-proxy:nanoserver-1803
-    docker push lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-nanoserver-1803
-    docker push lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-nanoserver-1803
-
     # nanoserver-1809
     docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-1809 lucaslorentz/caddy-docker-proxy:nanoserver-1809
     docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-1809 lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-nanoserver-1809
@@ -41,4 +37,12 @@ if [[ "${RELEASE_VERSION}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]]; then
     docker push lucaslorentz/caddy-docker-proxy:nanoserver-1809
     docker push lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-nanoserver-1809
     docker push lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-nanoserver-1809
+
+    # nanoserver-ltsc2022
+    docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-ltsc2022 lucaslorentz/caddy-docker-proxy:nanoserver-ltsc2022
+    docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-ltsc2022 lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-nanoserver-ltsc2022
+    docker tag lucaslorentz/caddy-docker-proxy:ci-nanoserver-ltsc2022 lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-nanoserver-ltsc2022
+    docker push lucaslorentz/caddy-docker-proxy:nanoserver-ltsc2022
+    docker push lucaslorentz/caddy-docker-proxy:${PATCH_VERSION}-nanoserver-ltsc2022
+    docker push lucaslorentz/caddy-docker-proxy:${MINOR_VERSION}-nanoserver-ltsc2022
 fi
--- tests/caddyfile+config/run.sh
+++ tests/caddyfile+config/run.sh
@@ -11,6 +11,9 @@ retry curl --show-error -s -k -f --resolve service.local:443:127.0.0.1 https://s
 retry curl --show-error -s -k -f --resolve caddyfile.local:443:127.0.0.1 https://caddyfile.local | grep caddyfile &&
 retry curl --show-error -s -k -f --resolve config.local:443:127.0.0.1 https://config.local | grep config ||
 {
+    echo "== Service errors =="
+    docker service ps --no-trunc caddy_test_caddy --format "{{.Error}}"
+    echo "== Service logs =="
     docker service logs caddy_test_caddy
     exit 1
 }
