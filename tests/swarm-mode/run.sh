e628f80f197af1e27b386a5fd47b068c380245e9
Juanjo Presa <juanjop@gmail.com>
2026-05-01 12:40:23 +0200

    test: show full whoami response in swarm-mode test

--- tests/swarm-mode/run.sh
+++ tests/swarm-mode/run.sh
@@ -7,7 +7,7 @@ set -e
 CONFIG_PREFIX="cdp-test-caddy_test"
 
 cleanup_configs() {
-  docker config ls --format "{{.Name}}" | grep "^${CONFIG_PREFIX}-" | xargs -r docker config rm || true
+  docker config ls --format "{{.Name}}" | grep "^${CONFIG_PREFIX}-" | xargs -r docker config rm >/dev/null || true
 }
 
 cleanup_configs
@@ -16,7 +16,7 @@ docker stack deploy -c compose.yaml --prune caddy_test
 
 retry bash -c "docker config ls --format '{{.Name}}' | grep '^${CONFIG_PREFIX}-'"
 
-retry curl --show-error -s -k -f --resolve service.local:443:127.0.0.1 https://service.local | grep Hostname ||
+retry curl --show-error -s -k -f --resolve service.local:443:127.0.0.1 https://service.local ||
 {
     echo "== Service errors =="
     docker service ps --no-trunc caddy_test_caddy --format "{{.Error}}"
